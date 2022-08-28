ce4d0f874e1265ffe00c9f0de4ad4fbf5a882235
xiaofeng.tang <xiaofeng.tang@centurygame>
2022-08-28 23:04:24 +0800

    项目初创

---
+++ include/Worker.h
@@ -0,0 +1,24 @@
+#pragma once
+
+using namespace std;
+
+class Worker
+{
+private:
+    /* data */
+public:
+    Worker(/* args */);
+    ~Worker();
+    int id;
+    int eachNum , //单次处理消息数
+    
+
+};
+
+Worker::Worker(/* args */)
+{
+}
+
+Worker::~Worker()
+{
+}
