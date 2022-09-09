04f57c2ce9d7da8b977abe26b87cd8611bc75cac
xiaofeng <1285013285@qq>
2022-09-09 16:06:43 +0800

    Changes
    Signed-off-by: xiaofeng <1285013285@qq>

--- include/socketworker.h
+++ include/socketworker.h
@@ -6,14 +6,11 @@ private:
     /* data */
 public:
     void init();
+    void operator()(); //线程函数
+    int efd; //操作epoll实例的 文件描述符
     socketworker(/* args */);
+    void onEvent(); //epoll 事件处理
     ~socketworker();
 };
 
-socketworker::socketworker(/* args */)
-{
-}
 
-socketworker::~socketworker()
-{
-}
--- src/socketworker.cpp
+++ src/socketworker.cpp
@@ -1,2 +1,26 @@
 #include "socketworker.h"
+#include <sys/epoll.h>
+#include <cassert>
 
+void
+socketworker::init() {
+    efd = epoll_create(1024);
+    assert(efd > 0);
+}
+
+void socketworker::operator()(){
+    const int EVENT_SIZE = 64;
+    struct epoll_event events[EVENT_SIZE];
+    while (true)
+    {
+        int event_num = epoll_wait(efd,events,EVENT_SIZE,-1);
+        assert(event_num >= 0);
+        for (int i = 0; i < event_num; i++)
+        {
+            epoll_event ev = events[i];
+            onEvent();
+        }
+    }
+}
+
+void socketworker::onEvent() {}
